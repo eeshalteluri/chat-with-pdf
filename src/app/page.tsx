f5bf339dd6c704426fd4c028d14eabed77e7b6fa
eeshalteluri <eeshalteluri@gmail.com>
2025-07-24 23:55:38 +0530

    File upload component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FileUpload from "@/components/FileUpload";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
@@ -25,7 +26,7 @@ export default async function Home() {
 
           <div className="w-full mt-4">
             {isAuth ? (
-              <h1>File upload</h1>
+              <FileUpload />
             ) : (
               <Link href="/sign-in">
                 <Button>
